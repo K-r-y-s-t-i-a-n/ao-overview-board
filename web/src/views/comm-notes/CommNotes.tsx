c3df026c2799edae8bf86439fb2638070d688252
Krystian <olkowski@outlook.com>
2023-06-12 11:51:41 +0100

    separates breeze

--- web/src/views/comm-notes/CommNotes.tsx
+++ web/src/views/comm-notes/CommNotes.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import Login from '../login';
+import axiosClient from '../../axios';
+import { Button } from '@mantine/core';
 
 const CommNotes = () => {
-  return <div>CommNotes</div>;
+  const getUser = async () => {
+    const user = await axiosClient.get('/api/user');
+    console.log(user);
+  };
+
+  const logout = async () => {
+    await axiosClient.post('/logout');
+    console.log('LOGGED OUT');
+  };
+
+  return (
+    <>
+      CommNotes
+      <Login />
+      <Button onClick={getUser}>GET USER</Button>
+      <Button onClick={logout}>LOGOUT</Button>
+    </>
+  );
 };
 
 export default CommNotes;
--- web/src/views/login/Login.tsx
+++ web/src/views/login/Login.tsx
@@ -1,7 +1,98 @@
-import React from 'react';
+import {
+  TextInput,
+  PasswordInput,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+  Box,
+} from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import axiosClient from '../../axios';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  const login = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    await axiosClient.get('/sanctum/csrf-cookie');
+    await axiosClient.post('/login', { email, password });
+    const user = await axiosClient.get('/api/user');
+    console.log(user);
+    setUser(user);
+    setLoading(false);
+  };
+
+  return (
+    <Box
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.background[1] : '#F7FAFC',
+        },
+      })}
+    >
+      <Container size={420} my={40}>
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `${theme.fontFamily}`,
+            fontWeight: 900,
+          })}
+          variant="gradient"
+        >
+          Overview Board
+        </Title>
+        <Text color="dimmed" size="sm" align="center" mt={5}>
+          Airways Optical Ltd.
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          {/* //* ----------------------- FORM ----------------------*/}
+          <form onSubmit={(e) => login(e)}>
+            <TextInput
+              label="Username "
+              placeholder="name.surname"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Your password"
+              required
+              mt="md"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Group position="right" mt="lg">
+              {/* <Checkbox label="Remember me" sx={{ lineHeight: 1 }} /> */}
+              <Anchor<'a'> onClick={(event) => event.preventDefault()} href="#" size="xs">
+                Forgot password?
+              </Anchor>
+            </Group>
+
+            <Button
+              fullWidth
+              mt="xl"
+              variant="gradient"
+              type="submit"
+              loaderPosition="left"
+              loading={loading}
+            >
+              Sign in
+            </Button>
+          </form>
+        </Paper>
+        <Paper>{JSON.stringify(user, undefined, 2)}</Paper>
+      </Container>
+    </Box>
+  );
 };
 
 export default Login;
